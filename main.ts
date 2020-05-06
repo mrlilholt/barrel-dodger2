f660ffe892f9cf5c1af9c42385348959c19d2592
mrlilholt <63080594+mrlilholt@users.noreply.github.com>
2020-05-06 18:16:29 -0400

    Update main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -19,52 +19,16 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 `
 }
+let projectile: Sprite = null
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
     game.over(false)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    let mySprite: Sprite = null
     if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
         mySprite.vy = -250
     }
 })
-let projectile: Sprite = null
-let mySprite: Sprite = null
-mySprite = sprites.create(img`
-. . . . . . 5 . 5 . . . . . . . 
-. . . . . f 5 5 5 f f . . . . . 
-. . . . f 1 5 2 5 1 6 f . . . . 
-. . . f 1 6 6 6 6 6 1 6 f . . . 
-. . . f 6 6 f f f f 6 1 f . . . 
-. . . f 6 f f d d f f 6 f . . . 
-. . f 6 f d f d d f d f 6 f . . 
-. . f 6 f d 3 d d 3 d f 6 f . . 
-. . f 6 6 f d d d d f 6 6 f . . 
-. f 6 6 f 3 f f f f 3 f 6 6 f . 
-. . f f d 3 5 3 3 5 3 d f f . . 
-. . f d d f 3 5 5 3 f d d f . . 
-. . . f f 3 3 3 3 3 3 f f . . . 
-. . . f 3 3 5 3 3 5 3 3 f . . . 
-. . . f f f f f f f f f f . . . 
-. . . . . f f . . f f . . . . . 
-`, SpriteKind.Player)
-mySprite.setPosition(20, 70)
-mySprite.ay = 500
-tiles.setTilemap(tiles.createTilemap(
-            hex`0a0008000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000303030303030303030303030303030303030303`,
-            img`
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 
-`,
-            [myTiles.tile0,sprites.builtin.brick,sprites.builtin.oceanDepths0,sprites.builtin.oceanDepths3],
-            TileScale.Sixteen
-        ))
-scene.setBackgroundColor(3)
 game.onUpdateInterval(1500, function () {
     projectile = sprites.createProjectileFromSide(img`
 3 e e e e e e 3 
